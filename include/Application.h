e71674bc43b0a9061c7a50f5ecc2ec0101050928
Toren Miller-Marcus <toren10000@gmail.com>
2026-07-27 14:07:06 -0700

    Linked SDL3 to .exe, creates resizable window displaying green square, added .gitattributes to let git automatically handle line endings

--- include/Application.h
+++ include/Application.h
@@ -1,6 +1,10 @@
 //Header for Application.cpp
 #pragma once
 
+struct SDL_Window;
+struct SDL_Renderer;
+
+
 class Application
 {
 public:
@@ -10,4 +14,11 @@ public:
     bool Initialize();
     void Run();
     void Shutdown();
+
+    private:
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    bool running;
 };
--- src/Application.cpp
+++ src/Application.cpp
@@ -1,8 +1,13 @@
 //Owns main loop and initializes SDL
 #include "Application.h"
+#include <SDL3/SDL.h>
+#include <iostream>
 
 Application::Application()
 {
+    window = nullptr;
+    renderer = nullptr;
+    running = false;
 }
 
 Application::~Application()
@@ -11,15 +16,80 @@ Application::~Application()
 
 bool Application::Initialize()
 {
+
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
+        return false;
+    }
+
+        
+        
+    window = SDL_CreateWindow(
+        "ECE103 Radar",
+        800,
+        600,
+        SDL_WINDOW_RESIZABLE
+    );
+
+    if (window == nullptr)
+    {
+        return false;
+    }
+
+
+    renderer = SDL_CreateRenderer(window, nullptr);
+
+
+    if (renderer == nullptr)
+    {
+        return false;
+    }
+
+
+    running = true;
+
     return true;
 }
 
 void Application::Run()
 {
-    return;
+     SDL_Event event;
+
+
+    while(running)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_EVENT_QUIT)
+            {
+                running = false;
+            }
+        }       
+
+
+        
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+
+        SDL_FRect rectangle;
+
+        rectangle.x = 350;
+        rectangle.y = 250;
+        rectangle.w = 100;
+        rectangle.h = 100;
+
+        SDL_RenderFillRect(renderer, &rectangle);
+
+        SDL_RenderPresent(renderer);
+     
+    }
 }
 
 void Application::Shutdown()
 {
-    return;
+    SDL_DestroyRenderer(renderer);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
 }
